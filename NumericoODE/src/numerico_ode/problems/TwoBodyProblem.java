6b56be8fdb509f33c9bd436e4d1e2338424e7731
Mellandd <joseluis.mellinaa@um.es>
2020-11-07 12:31:26 +0100

    Problema de 2 cuerpos terminado.

--- NumericoODE/src/numerico_ode/problems/TwoBodyProblem.java
+++ NumericoODE/src/numerico_ode/problems/TwoBodyProblem.java
@@ -1,5 +1,6 @@
 package numerico_ode.problems;
 
+import numerico_ode.interpolation.*;
 import numerico_ode.methods.*;
 import numerico_ode.ode.*;
 import numerico_ode.tools.*;
@@ -37,19 +38,53 @@ public class TwoBodyProblem implements InitialValueProblem{
 		FixedStepMethod method3 = new FixedStepModifiedEulerMethod(problem, 1);
 		
         NumericalSolutionPoint previousPoint, currentPoint;
-        //method.solve(9000);
+        
+        //Obtenemos las órbitas para euler y euler modificado.
+        method2.solve(9000); 
+        method3.solve(9000);
+        DisplaySolution.statePlot(method2.getSolution(), 0, 2);
+        DisplaySolution.statePlot(method3.getSolution(), 0, 2);
+        
+        //Empezamos a calcular el perifelio con runge-kutta.
         previousPoint = currentPoint = method.getSolution().getLastPoint();
-        double x0 = currentPoint.getState(0);
-        currentPoint = method.step();
-        double tolerance = 5e-2;
-        //double y = previousPoint.getState(2);
-        while (Math.pow(currentPoint.getState(0)-x0,2) + Math.pow(currentPoint.getState(2),2)>tolerance) {
+        while (currentPoint.getState(2)>=0) {
             previousPoint = currentPoint;
             currentPoint = method.step();
         }
         previousPoint.println();
         currentPoint.println();
-        DisplaySolution.statePlot(method.getSolution(), 0, 2);
+        
+        //Ahora interpolamos para hallar exactamente el 0.
+        StateFunction interpolator = new EulerMethodInterpolator(problem, previousPoint);
+        double zeroYAt = BisectionMethod.findZero(interpolator, previousPoint.getTime(), currentPoint.getTime(), 1.0e-8, 2);
+        if (Double.isNaN(zeroYAt)) {
+        	System.out.println("Perihelion not found!");
+        } else {
+        	double periphelio = interpolator.getState(zeroYAt, 0);
+        	System.out.println("Perihelion at x= "+periphelio);
+        }
+        
+        //DisplaySolution.statePlot(method.getSolution(), 0, 2); //Gráfica que muestra de afelio a perifelio
+        
+        //Seguimos ahora para obtener la vuelta completa y calcular el año.
+        while(currentPoint.getState(2)<0) {
+            previousPoint = currentPoint;
+            currentPoint = method.step();
+        }
+        previousPoint.println();
+        currentPoint.println();
+        
+        //Interpolamos para obtenerlo exactamente.
+        StateFunction interpolator2 = new EulerMethodInterpolator(problem, previousPoint);
+        double zeroYAt2 = BisectionMethod.findZero(interpolator2, previousPoint.getTime(), currentPoint.getTime(), 1.0e-8, 2);
+        if (Double.isNaN(zeroYAt2)) {
+        	System.out.println("Orbit not found");
+        } else {
+        	System.out.println("Year at: "+ zeroYAt2/24. +" days");
+        }
+        
+        DisplaySolution.statePlot(method.getSolution(), 0, 2); //Obtenemos la órbita completa con Runge-Kutta
+        
 	}
 
 }
